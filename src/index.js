2ef216d5df85234ba330dd7c55e6ef922d481430
axantroff <ilyakaltovich7@gmail.com>
2020-10-01 14:58:07 +0300

    feat: add max,min,avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) return 0
+  if(array.length > 0) {
+  //   return Math.max(...array)
+  // } else {
+  //   return 0
+      let min = array[0];
+      for (let i = 1; i < array.length; ++i) {
+        if (array[i] < min) {
+          min = array[i];
+        }
+      }
+      return min;
+  } else return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined) return 0
+    if(array.length > 0) {
+          let max = array[0];
+          for (let i = 1; i < array.length; ++i) {
+            if (array[i] > max) {
+              max = array[i];
+            }
+          }
+          return max;
+      } else return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) return 0
+  if(array.length > 0) {
+    let total = 0;
+    for(let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    let avg = total / array.length;
+    return avg;
+    } else {
+        return 0
+    }
 }
